d4ec2855202c23ce4de6c07b486678bd55e77138
Quang Tran <tranminhquang0907@gmail.com>
2019-11-16 12:48:17 +0700

    change style in menu-item

--- src/components/menu-item/menu-item.styles.jsx
+++ src/components/menu-item/menu-item.styles.jsx
@@ -1,27 +1,27 @@
 import styled from 'styled-components';
 
 export const MenuItemContainer = styled.div`
-	height: ${({ size }) => (size ? '380px' : '240px')}
-	min-width: 30%;
-	overflow: hidden;
-	flex: 1 1 auto;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	border: 1px solid black;
-	margin: 0 7.5px 15px;
-	overflow: hidden;
-	&:hover {
-		cursor: pointer;
-		& .background-image {
-			transform: scale(1.1);
-			transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
-		}
-		& .content {
-			opacity: 0.9;
-		}
-	}
-	&:first-child {
+  height: ${({ size }) => (size ? '380px' : '240px')};
+  min-width: 30%;
+  overflow: hidden;
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  margin: 0 7.5px 15px;
+  overflow: hidden;
+  &:hover {
+    cursor: pointer;
+    & .background-image {
+      transform: scale(1.1);
+      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+    }
+    & .content {
+      opacity: 0.9;
+    }
+  }
+  &:first-child {
     margin-right: 7.5px;
   }
   &:last-child {
@@ -39,6 +39,7 @@ export const BackgroundImageContainer = styled.div`
 
 export const ContentContainer = styled.div`
   height: 90px;
+  width: 120px;
   padding: 0 25px;
   display: flex;
   flex-direction: column;
@@ -50,9 +51,9 @@ export const ContentContainer = styled.div`
   position: absolute;
 `;
 
-export const ContentTitle = styled.span`
+export const ContentTitle = styled.h1`
   font-weight: bold;
-  margin-bottom: 6px;
+  margin: 0 6px;
   font-size: 22px;
   color: #4a4a4a;
 `;
