db14bff7a52096f40197fe8e95625003a91826b2
Quang Tran <tranminhquang0907@gmail.com>
2019-11-26 17:28:33 +0700

    run compression in production

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const path = require('path'); //Lets us build out pathing for our directories
 const compression = require('compression');
 const enforce = require('express-sslify');
@@ -12,14 +11,14 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
-app.use(bodyParser.json()); //parse input
-app.use(bodyParser.urlencoded({ extended: true })); //urlencoded is a way for us to make sure that the URL strings we're getting in and we're passing out do not contain things lkke spaces or symbols
+app.use(express.json()); //parse input
+app.use(express.urlencoded({ extended: true })); //urlencoded is a way for us to make sure that the URL strings we're getting in and we're passing out do not contain things lkke spaces or symbols
 
 app.use(cors()); //Cross-Origin Request so we're actually able to properly make requests to our backend server
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(compression());
+  app.use(enforce.HTTPS({ trustProtoHeader: true })); //For
   app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', function(req, res) {
     //For every route that is not covered by the future route we write
