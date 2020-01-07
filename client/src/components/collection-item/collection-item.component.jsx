bd53efbf7e311f915f23e94aecc0b56bf3a67b51
Quang Tran <tranminhquang0907@gmail.com>
2020-01-07 20:20:59 +0700

    implemented a function to prevent unauthenticated user from adding item to cart

--- client/src/components/collection-item/collection-item.component.jsx
+++ client/src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import { addItem } from '../../redux/cart/cart.actions';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 import {
   CollectionItemContainer,
   CollectionFooterContainer,
@@ -10,7 +12,7 @@ import {
   PriceContainer
 } from './collection-item.styles';
 
-const CollectionItem = ({ item, addItem }) => {
+const CollectionItem = ({ item, addItem, currentUser }) => {
   const { name, price, imageUrl } = item;
 
   return (
@@ -20,21 +22,34 @@ const CollectionItem = ({ item, addItem }) => {
         <NameContainer>{name}</NameContainer>
         <PriceContainer>${price}</PriceContainer>
       </CollectionFooterContainer>
-      <AddButton
-        onClick={
-          () => addItem(item)
-          //We can't use onClick={addItem(item)} because the way javascript runs, when it hits this line it's just going to evaluate addItem(item) immediately instead of triggering it whenever element with the onClick is actually clicked.
-        }
-        inverted
-      >
-        Add to cart
-      </AddButton>
+      {currentUser ? (
+        <AddButton
+          onClick={
+            () => addItem(item)
+            //We can't use onClick={addItem(item)} because the way javascript runs, when it hits this line it's just going to evaluate addItem(item) immediately instead of triggering it whenever element with the onClick is actually clicked.
+          }
+          inverted
+        >
+          Add to cart
+        </AddButton>
+      ) : (
+        <AddButton
+          onClick={() => alert('Please sign in to add item to cart!')}
+          inverted
+        >
+          Add to cart
+        </AddButton>
+      )}
     </CollectionItemContainer>
   );
 };
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+});
 
 const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item))
 });
 
-export default connect(null, mapDispatchToProps)(CollectionItem);
+export default connect(mapStateToProps, mapDispatchToProps)(CollectionItem);
